7c8874b14faa90c3e997277234ac2c8fdf44af5c
quequeg <bwd111222@outlook.com>
2025-10-24 18:36:51 +0800

    Add files via upload

---
+++ text reader.py
@@ -0,0 +1,10 @@
+text = '''This is a text reader made by quequeg
+v1.0.0'''
+print(text)
+read1 = "this is a text"
+book = ["read1",]
+print(book)
+keyin = input("please type the title of the book you want to read:")
+print(keyin)
+if keyin == "read1":
+    print(read1)
